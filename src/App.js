ea383769f3ab78a2f178a360308513ee52f00502
José Antonio dos Santos Neto <joseantonio.neto@outlook.com>
2020-03-29 00:11:31 -0300

    Atualização de packages do npm e ajuste na configuração da url da API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
-import Main from "./pages/Main";
-
-import Routes from "./routes";
+import Routes from './routes';
 
 class App extends Component {
-  render() {
-    return <Routes />;
-  }
+    render() {
+        return <Routes />;
+    }
 }
-
-// const App = () => <Routes />;
-
-/* function App() {
-  return <Main />;
-} */
 
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://netobox-backend.herokuapp.com',
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
